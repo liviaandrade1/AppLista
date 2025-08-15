2e1ca73a38a903f1ae6b6079b491aee7b28e07c9
liviaandrade1 <livia.oliveira166@etec.sp.gov.br>
2025-08-15 20:06:18 -0300

    lista de itens

--- app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaProdutosActivity extends AppCompatActivity {
+
+    RecyclerView idListaProdutos;
+    List<Produtos> lstProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,18 @@ public class ListaProdutosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //criando array q vai ter a lista de produtos
+        lstProdutos = new ArrayList<>();
+        lstProdutos.add(new Produtos("Hello Kitty pequena", R.drawable.img1));
+        lstProdutos.add(new Produtos("Capivara de chapéu", R.drawable.img6));
+        lstProdutos.add(new Produtos("Sapinho Sanrio", R.drawable.img5));
+        lstProdutos.add(new Produtos("Pou", R.drawable.img7));
+        lstProdutos.add(new Produtos("Tartaruga", R.drawable.img11));
+        lstProdutos.add(new Produtos("Porquinho", R.drawable.img13));
+        lstProdutos.add(new Produtos("Morcego", R.drawable.img10));
+        lstProdutos.add(new Produtos("Hello Kitty pequena", R.drawable.img1));
+        lstProdutos.add(new Produtos("Galinha Pintadinha", R.drawable.img1));
+        idListaProdutos = findViewById(R.id.idListaProdutos);
     }
 }
